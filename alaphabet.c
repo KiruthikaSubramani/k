60bbecf63a112c681dce5701d99344fba1cd296a
KiruthikaSubramani <30343971+KiruthikaSubramani@users.noreply.github.com>
2017-08-12 16:59:11 +0530

    Create alaphabet.c

---
+++ alaphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+printf("enter the character");
+scanf("%c",&s);
+if((a>='s'&&z<='s')||(A>='s'&&Z<='s'))
+{
+printf("the given letter is alphabet")
+}
+else
+{
+printf("the given letter is not alphabet");
+}
+getch();
+}
